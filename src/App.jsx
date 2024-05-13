aa4b4744689c62443985f91fc194d879c9ae4061
vjcodes <47989148+vjcodes@users.noreply.github.com>
2024-05-14 01:12:17 +0530

    removed unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import { useCustomEffect } from "./hooks/useCustomEffect";
 
 const App = () => {
   const [counter1, setCounter1] = useState(0);
-  const [counter2, setCounter2] = useState(0);
 
   useCustomEffect(() => {
     console.log("use effect triggered", counter1);
@@ -19,7 +18,7 @@ const App = () => {
     <div>
       <div>{counter1}</div>
       <button onClick={() => setCounter1(counter1 + 1)}>+</button>
-      <button onClick={() => setCounter1(counter2 - 1)}>-</button>
+      <button onClick={() => setCounter1(counter1 - 1)}>-</button>
     </div>
   );
 };
